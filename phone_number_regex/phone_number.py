6ed4658c808988fc6aac69461bbba3a436ba6e78
MinhD <dovminh@gmail.com>
2020-08-10 15:06:46 -0700

    Fixing regex for number to not include Hettie

--- phone_number_regex/phone_number.py
+++ phone_number_regex/phone_number.py
@@ -2,7 +2,7 @@ import re
 
 def find_US_numbers(filename):
 
-	pattern = re.compile(r"(?:[0-9]{3}-){2}[0-9]{4}\b|(?:[0-9]{3}\.){2}[0-9]{4}\b|(?:[0-9]{3} ){2}[0-9]{4}\b|(?:\([0-9]{3}\) [0-9]{3}(?: |-)[0-9]{4})")
+	pattern = re.compile(r"(?:[0-9]{3}-){2}[0-9]{4}\b|(?:[0-9]{3}\.){2}[0-9]{4}\b|\s(?:[0-9]{3} ){2}[0-9]{4}\b|(?:\([0-9]{3}\) [0-9]{3}(?: |-)[0-9]{4})")
 	valid_num_dict = dict()
 
 	with open(filename, 'r') as file:
@@ -10,7 +10,8 @@ def find_US_numbers(filename):
 			columns = line.split()
 			if re.search(pattern, line):
 				m = re.search(pattern, line)
-				valid_num_dict[columns[0]] = m.group(0)
+				number = m.group(0)
+				valid_num_dict[columns[0]] = number.lstrip("\t")
 
 	return(valid_num_dict)
 
